fdec54fc2e75f26747e20097aed2d80f00aff704
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-26 17:05:29 -0300

    Update reset.js

--- Commands/reset.js
+++ Commands/reset.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  if(message.author.id != "308352017263493125")return message.reply("Comando solo de <@308352017263493125>");
 message.channel.send("Reiniciando...");
 bot.destroy();
 bot.login(prcess.env.BOT_TOKEN);
