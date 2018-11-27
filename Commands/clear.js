7119de0d49fb8e032f7e89f4ef9a86ee5098fec3
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-27 19:19:10 -0300

    Create clear.js

---
+++ Commands/clear.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES"))return message.reply("Sueña, lol");
+  if(!args[0])return message.channel.send("Especifica la cantidad");
+  message.channel.bulkDelete(args[0]).then(() =>
+message.channel.send(`Eliminados ${args[0]} mensajes`));
+//message.delete().catch();
+}
+
+module.exports.help = {
+  name: "clear"
+}
