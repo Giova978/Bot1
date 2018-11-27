7b05e5de71eb01f8334e31ed1732747138e9bbf8
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-27 18:03:22 -0300

    Update Stop.js

--- Commands/Stop.js
+++ Commands/Stop.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if(message.author.id != "308352017263493125")return message.reply("Comando solo de <@308352017263493125>");
-message.channel.send("Apgando...");
+message.channel.send("Apagando...");
 bot.destroy();
 }
 
