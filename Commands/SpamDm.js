9071d5457c516249378619ac49d74e35fea83b14
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-27 19:05:50 -0300

    Update SpamDm.js

--- Commands/SpamDm.js
+++ Commands/SpamDm.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot, message, args) => {
   //if(message.author.id != "308352017263493125")return message.reply(`Comando en pruebas, solo <@308352017263493125>`);
   let sUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!sUser)return message.reply("Usuario no encontrado");
+  if(sUser.id === "308352017263493125")return message.reply("No puedes hacerle eso a mi creador");
   let veces = parseInt(args[1],10);
   if(!veces)return message.reply("Cantidad no especificada");
   if(veces > 99)return message.reply("La cantidad debe ser menor a 99");
@@ -12,13 +13,14 @@ module.exports.run = async (bot, message, args) => {
   if(!mensaje)return message.reply("Mensaje no especificado");
 
   var i = 0;
-  console.log(i);
+  //console.log(i);
   for(i = 0;i < veces;i++) {
     try{
   await sUser.send(mensaje);
 }catch{
-  message.channel.send(`<@${sUser.id}>,${mensaje}`);
+  message.channel.send(`<@${sUser.id}>,${mensaje}.Tienes los dms bloqueados`);
 }
+message.reply("Listo, spam echo");
 };
 
 };
