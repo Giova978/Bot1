67f03cf64456e1dbe20c31566c0593cfab105595
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-29 16:43:15 -0300

    Fix

--- Commands/unmute.js
+++ Commands/unmute.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.expors.run = async (bot, message, args) =>{
-  if(!message.author.hasPermission("KICK_MEMBERS" || "ADMINISTRATOR" || "BAN_MEMBERS")return message.reply("No eres admin");
+  if(!message.author.hasPermission("KICK_MEMBERS" || "ADMINISTRATOR" || "BAN_MEMBERS"))return message.reply("No eres admin");
   let mRole = message.guild.roles.find(`name`, "muted");
   let user = message.guild.member(message.mentions.users.first() || message.guild.member.get(arg[0]));
   if(!user)return message.reply("Usuario no encontrado");
