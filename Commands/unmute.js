e61f6ad3a4b41432bbf76ffa6c32c8ba9ae903c1
Giova978 <42687995+Giova978@users.noreply.github.com>
2018-11-29 16:52:59 -0300

    Fix

--- Commands/unmute.js
+++ Commands/unmute.js
@@ -5,9 +5,10 @@ module.exports.run = async (bot, message, args) => {
   let mRole = message.guild.roles.find(`name`, "muted");
   let user = message.guild.member(message.mentions.users.first() || message.guild.member.get(arg[0]));
   if(!user)return message.reply("Usuario no encontrado");
+  if(!user.roles.has(mRole.id))return message.reply("No esta muteado");
   user.removeRole(mRole);
 }
 
-module.exports.help ={
+module.exports.help = {
   name: "unmute"
 }
